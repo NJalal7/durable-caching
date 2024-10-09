17e33e840c3b8b179e0d9b8b55cae9f996167826
Nahrin Jalal <jalal.nahrin@gmail.com>
2024-10-09 12:48:56 -0300

    implement dad joke api

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -5,11 +5,33 @@
 // You can develop and deploy serverless functions right here as part
 // of your site. Netlify Functions will handle the rest for you.
 
+async function joke() {
+    try {
+        const response = await fetch("https://icanhazdadjoke.com/", {
+          headers: {
+            "Accept": "application/json" // Specify JSON format
+          }
+        });
+    
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+    
+        const jokeData = await response.json();
+        return jokeData.joke;
+      } catch (error) {
+        console.error("Failed to fetch joke:", error);
+        return "Error: Could not retrieve a joke.";
+      }
+} 
 
 exports.handler = async event => {
-    const subject = event.queryStringParameters.name || 'World'
+    const subject = await joke();
     return {
         statusCode: 200,
         body: `Hello ${subject}!`,
+        headers: {
+			'Netlify-CDN-Cache-Control': 'public, durable, max-age=60, stale-while-revalidate=120'
+		}
     }
 }
